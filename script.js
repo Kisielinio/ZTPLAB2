7a5496557366f0fffc17b557f0b2dbe9e749dc8b
Michal Motowidlo <motowidlo94@gmail.com>
2024-10-19 11:07:52 +0200

    Base

---
+++ script.js
@@ -0,0 +1,9 @@
+// Pobieramy elementy z DOM
+const textarea = document.getElementById('textarea');
+const charCount = document.getElementById('charCount');
+
+// Nasłuchiwanie na zmianę w polu tekstowym
+textarea.addEventListener('input', () => {
+    // Aktualizowanie licznika znaków na podstawie długości tekstu
+    charCount.textContent = textarea.value.length;
+});
